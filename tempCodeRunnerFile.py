d7d110f5b9340463046abcbdc96e99204510f3ba
Raymond Zheng <163009036+snowray23@users.noreply.github.com>
2024-04-08 14:25:15 -0400

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-def new_contact(contacts):
-    name = input("Enter name: ")
-    phone = input("Enter your phone number")
-    email = input("Enter your email")
-    additional_info = input("any additonal information")
-    contacts[email] = {name, phone, email, additional_info}
-    print("new contact added")
-
- 
